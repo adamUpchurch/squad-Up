38bf26fff8cb7c8de5d621426d8c54a180f17da7
Adam Upchurch <aupchurch167@gmail.com>
2018-10-18 19:22:37 -0400

    screw the menu button and logo formatting - fixing the formatting on main page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import Menu from './Menu/Menu.js';
 
 class Header extends Component {
 
   render() {
     return(
-      <header className="App-header">
-        <h1 className="App-title">Live Stream your Squad</h1>
+      <header style={{  height: '5%', padding: '1px',
+        color: 'white'}}>
+        <div className="row">
+          <h1 style={{fontWeight: '600', fontSize: '2em'}}>Squad Up</h1>
+        </div>
       </header>
     )
   }
